e4c55a82320b69b6e89e575cd9169231333991b0
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2026-03-07 23:54:13 +0100

    Add test project config and base test feature

---
+++ UtilityNGPKG.Test/config/BaseTestFeature.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityNGPKG.ExternalApiIntegration;
+using UtilityNGPKG.FilePost;
+using UtilityNGPKG.KYC;
+using UtilityNGPKG.Mailer;
+using UtilityNGPKG.PaymentGateway.Paystack;
+using UtilityNGPKG.Sanitizer;
+using UtilityNGPKG.Tokenomics;
+
+namespace UtilityNGPKG.Test.config
+{
+    internal class BaseTestFeature
+    {
+        protected readonly IPaystackService paystackService;
+        protected readonly IMailService mailService;
+        protected readonly ISanitizationService sanitizationService;
+        protected readonly ITokenBuilder tokenBuilder;
+        protected readonly IKycService kycService;
+        protected readonly IFileService fileService;
+        protected readonly IApiIntegrationService apiIntegrationService;
+
+        protected BaseTestFeature()
+        {
+            paystackService = StartUp.Resolve<IPaystackService>();
+            mailService = StartUp.Resolve<IMailService>();
+            sanitizationService = StartUp.Resolve<ISanitizationService>();
+            tokenBuilder = StartUp.Resolve<ITokenBuilder>();
+            kycService = StartUp.Resolve<IKycService>();
+            fileService = StartUp.Resolve<IFileService>();
+            apiIntegrationService = StartUp.Resolve<IApiIntegrationService>();
+        }
+    }
+}
---
+++ UtilityNGPKG.Test/config/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UtilityNGPKG.Test.config
+{
+    internal class StartUp
+    {
+        private static IServiceProvider provider;
+        public StartUp()
+        {
+            provider = Build();
+        }
+        public static IServiceProvider Build()
+        {
+            var services = new ServiceCollection();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config/testconfig.json", false, true)
+                .Build();
+
+            services.AddUtilityNGPKG();
+            return services.BuildServiceProvider();
+        }
+
+        public static T Resolve<T>() where T : notnull => provider.GetRequiredService<T>();
+    }
+}
